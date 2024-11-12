90857bfd3be82ce4184c7df43c4cad62cbe67393
luc1236060 <lau2000g@gmail.com>
2024-11-12 16:48:38 -0500

    Update louis.js

--- backend/louis.js
+++ backend/louis.js
@@ -2,18 +2,14 @@
 app.post("/api/HealthInfo", async (req, res, next) => 
     {
 
-    const {id, height, weight, bmi, calories} = req.body;
+    let {id, height, weight, bmi, calories} = req.body;
     var error = '';
     try {
 
-        height = parseFloat(height);
-        weight = parseFloat(weight);
-        bmi = parseFloat(bmi);
-        calories = parseFloat(calories);
-
-        if (isNaN(height) || isNaN(weight) || isNaN(bmi) || isNaN(calories)){
-            throw new Error("Invalid format");
-        }
+        if(height != 0) {height = parseFloat(height); if(isNaN(height)){throw new Error("Invalid format");}}
+        if(weight != 0) {weight = parseFloat(weight); if(isNaN(weight)){throw new Error("Invalid format");}}
+        if(bmi != 0) {bmi = parseFloat(bmi); if(isNaN(bmi)){throw new Error("Invalid format");}}
+        if(calories != 0) {calories = parseFloat(calories); if(isNaN(calories)){throw new Error("Invalid format");}}
 
         const db = client.db();
         const result = await db.collection('HealthInfo').updateOne( {UserID: id}, 
