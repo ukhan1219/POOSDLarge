25ea1aa111355791717be7bbab2b875da614f475
Leo Salazar <leosalazar137@gmail.com>
2024-11-18 22:28:17 -0500

    forgot to put landing page stuff on landing page lol

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import CardPage from './pages/CardPage';
+
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
