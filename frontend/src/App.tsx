3f0039d119a2c620d0b1e44baa9f3e16c5cf5257
ukhan1219 <113568605+ukhan1219@users.noreply.github.com>
2024-11-22 00:21:51 -0500

    im gonna die

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,25 @@
-// import LandingPage from './pages/LandingPage';
-import Dashboard from './pages/Dashboard';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
+import LandingPage from "./pages/LandingPage";
+import RegisterPage from "./pages/RegisterPage";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  const LoggedIn = true
+  const LoggedIn = true; // Adjust this logic as needed to determine user login state
 
   return (
-    < >
-      {/* <LandingPage status={LoggedIn}/> */}
-      <Dashboard status={LoggedIn}/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        {LoggedIn && (
+          <Route path="/dashboard" element={<Dashboard status={LoggedIn} />} />
+        )}
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/LoginPage/index.tsx';
-import './styles.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./styles.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    {/* <Dashboard /> */}
-    <Router >
-      <Routes>
-        {<Route path="/login" element={<LoginPage />} />}
-        {/* <Route path="/cards" element={<CardPage />} /> */}
-      </Routes>
-    </Router>
   </StrictMode>,
-)
+);
